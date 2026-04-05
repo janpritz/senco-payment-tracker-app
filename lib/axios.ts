09caad0c7ce2245291d44d9e4ea0161c368b91a8
janpritz <academics.johnfritzcabalhin@gmail.com>
2026-04-05 17:08:05 +0800

    fix axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.accsangkaychatbot.com',
     withCredentials: true, // MANDATORY for cookies
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
